b411ede30d2fbe4f701353c03c02a85a20c6ff7d
Ruris <ruris@qq.com>
2021-10-08 17:20:28 +0800

    Update LLCalendarMonthViewModel.swift

--- LLCalendar/Classes/ViewModel/LLCalendarMonthViewModel.swift
+++ LLCalendar/Classes/ViewModel/LLCalendarMonthViewModel.swift
@@ -22,23 +22,21 @@ open class LLCalendarMonthViewModel {
         guard var firstDate = calendar.date(from: DateComponents(year: year)) else {
             return
         }
-//        print(firstDate)
-//        
-        let formatter = DateFormatter()
+////        print(firstDate)
+////
+//        let formatter = DateFormatter()
+////
+//        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEEE"
 //
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEEE"
-        
         var months: [LLCalendar.Month] = []
 
         calendar.range(of: .month, in: .year, for: firstDate)?.forEach({ month in
             var days: [LLCalendar.Day] = []
             let weekday = calendar.component(.weekday, from: firstDate) - 1
-            
-            print(formatter.string(from: firstDate), weekday)
-            
             calendar.range(of: .day, in: .month, for: firstDate)?.forEach({ day in
                 days.append(LLCalendar.Day(name: "\(calendar.component(.day, from: firstDate))"))
-                firstDate.addTimeInterval(3600 * 24)
+                /// 增加一天 `86400 = 3600 * 24`
+                firstDate.addTimeInterval(86400)
             })
             months.append(LLCalendar.Month(offset: weekday, days: days))
         })
